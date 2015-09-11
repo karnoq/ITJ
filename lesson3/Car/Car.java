d1ed8af09c00ea00e25827b625d81d6ea016faa1
karnoq <smith.sheldon@gmail.com>
2015-09-11 15:25:23 -0600

    Initial commit for lesson3

---
+++ lesson3/Car/Car.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Car here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car
+{
+    private double milesDriven;
+    private double gasInTank;
+    
+    public void drive(double distance)
+    {
+        milesDriven = milesDriven + distance;
+    }
+    
+    public void addGas(double amount)
+    {
+        gasInTank = gasInTank + amount;
+    }
+    
+    // More methods...
+}
