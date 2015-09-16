142470bb1c80eabfe5fede4f9c70169593ef4fb8
karnoq <smith.sheldon@gmail.com>
2015-09-15 20:32:50 -0600

    Added some formatted printing

--- lesson4/formattedOutput/MicroKitchenTable.java
+++ lesson4/formattedOutput/MicroKitchenTable.java
@@ -17,8 +17,8 @@ public class MicroKitchenTable
         // TODO: Print entries for Sara
         // Be sure to line up the entries in the columns using printf
         // Print floating-point numbers with two digits after the decimal point
-
+        System.out.printf("%8d|%8.2f|%s\n", saraCookiesPerDay, saraCerealPerDay, saraName);
         // TODO: Print entries for Cay
-
+        System.out.printf("%8d|%8.2f|%s\n", cayCookiesPerDay, cayCerealPerDay, cayName);
     }
 }
