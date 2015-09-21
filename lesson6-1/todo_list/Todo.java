b3f2231d822334682db7d1088f1e2bd13061da75
karnoq <smith.sheldon@gmail.com>
2015-09-21 14:14:32 -0600

    Created todo_list

---
+++ lesson6-1/todo_list/Todo.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Todo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Todo
+{
+    public static void main(String args[])
+    {
+        int counter = 1;
+    
+        while (counter <= 6)
+        {
+            System.out.println(counter + ". Sleep");
+            counter++;
+        }
+    }
+}
