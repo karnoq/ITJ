e8cec8c9232e5b83d003ed76f419cd7068c5849a
karnoq <smith.sheldon@gmail.com>
2015-09-21 14:47:39 -0600

    Completed the Twitterizer class

--- lesson6-1/twitter/Twitterizer.java
+++ lesson6-1/twitter/Twitterizer.java
@@ -4,9 +4,29 @@ public class Twitterizer
     /**
      * Shortens and prints longPost by removing vowels
      * @param longPost the post whose vowels need to be removed.
+     * @return string with vowels removed.
      */
     public String shorten(String longPost) 
     {
         // YOUR CODE HERE
+        String post = longPost;
+        int stringLength = post.length();
+        
+        String shortPost = "";
+        
+        for (int i = 0; i < stringLength; i++)
+        {
+            String postSubString = post.substring(i, i+1);
+            if (!"aeiouAEIOU".contains(postSubString))
+            {
+                shortPost = shortPost + post.substring(i, i+1);
+            }
+            else if (postSubString == " ")
+            {
+                shortPost = shortPost + " ";
+            }
+        }
+        
+        return shortPost;
     }
 }
--- lesson6-1/twitter/TwitterizerTester.java
+++ lesson6-1/twitter/TwitterizerTester.java
@@ -1,4 +1,4 @@
-
+ 
 public class TwitterizerTester
 {
     public static void main(String[] args)
