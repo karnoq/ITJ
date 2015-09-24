197186067beb214247c0b5710275560b51f2c622
karnoq <smith.sheldon@gmail.com>
2015-09-24 14:05:04 -0600

    Online version and download version differ. Added the solutionfrom the online version and commented it out.

--- lesson6-2/chevalier_de_mere/TwoDiceTwentyFourTimes.java
+++ lesson6-2/chevalier_de_mere/TwoDiceTwentyFourTimes.java
@@ -25,3 +25,58 @@ public class TwoDiceTwentyFourTimes
         }
     }
 }
+
+// public class DiceGame
+// {
+//     Random generator;
+//     
+//     public DiceGame()
+//     {   
+//         generator = new Random(45);
+//     }
+//     /** 
+//      * Throw a die four times and bet on at least one 6. 
+//      * @return true if the chevalier wins. 
+//      */
+//     public boolean game1()
+//     {
+//         // YOUR CODE HERE
+//         // Use the instance variable generator to simulate 
+//         // 4 die rolls. 
+//         // Return true if the chevalier wins (at least one is a 6).
+//         boolean result = false;
+//         
+//         for (int i = 0; i < 4; i++)
+//         {
+//             int roll = generator.nextInt(6) + 1;
+//             
+//             if (roll == 6)
+//                 result = true;
+//         }
+//         return result;
+//     }
+//     
+//     /**
+//      * Throw two dice 24 times and bet on at least one double-six.
+//      * @return true if the chevalier wins. 
+//      */
+//     public boolean game2()
+//     {   
+//         // YOUR CODE HERE 
+//         // Use the instance variable generator to simulate 
+//         // 24 rolls of a pair of dice.
+//         // Return true if at least one pair is both 6s. 
+//         boolean result = false;
+//         
+//         for (int i = 0; i < 24; i++)
+//         {
+//             int die1 = generator.nextInt(6) + 1;
+//             int die2 = generator.nextInt(6) + 1;
+//             
+//             if (die1 == 6 && die2 == 6)
+//                 result = true;
+//         }
+//         
+//         return result;
+//     }
+// }
