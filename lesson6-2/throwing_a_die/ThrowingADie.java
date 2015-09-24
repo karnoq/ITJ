d88084ddd6902c5447d6f5cad351676472e9a424
karnoq <smith.sheldon@gmail.com>
2015-09-24 13:53:58 -0600

    Counting random throws of a die; printing results

--- lesson6-2/throwing_a_die/ThrowingADie.java
+++ lesson6-2/throwing_a_die/ThrowingADie.java
@@ -6,11 +6,27 @@ public class ThrowingADie
 {
     public static void main(String[] args)
     {
-        Random generator = new Random();
+        Random generator = new Random(42);
         // YOUR CODE HERE
         // Write a loop that simulates 100 die rolls. 
         // generate 100 random ints using generator. 
         // the ints should have values 1,2,3,4,5, or 6
         // Print the number of 6s rolled. 
+        int count = 0;
+        int tries = 100;
+        int min = 1;
+        int max = 6;
+        
+        for (int i = 0; i <= tries; i++)
+        {
+            int roll = generator.nextInt((max - min) + 1) + min;
+            
+            if (roll == max)
+            {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
     }
 }
