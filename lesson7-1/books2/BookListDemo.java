06f15bb31aab979102db10052706fa425f0035e6
karnoq <smith.sheldon@gmail.com>
2015-09-29 11:32:36 -0600

    Lesson on the add method for arrays

--- lesson7-1/books2/BookListDemo.java
+++ lesson7-1/books2/BookListDemo.java
@@ -22,6 +22,7 @@ public class BookListDemo
         // Replace The Eyre Affair with
         // Lost in a Good Book
         // you can use the variable sequel that I defined above.
+        booksToRead.add(2, anotherBook);
 
         // Please don't modify the following line:
         System.out.println(booksToRead);
