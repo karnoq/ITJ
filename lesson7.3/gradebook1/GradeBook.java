55081bc14c24eddaa7e7a0add94080d8a10c5dbd
karnoq <smith.sheldon@gmail.com>
2015-10-03 11:35:40 -0600

    Added the udacity solution to the gradebook

--- lesson7.3/gradebook1/GradeBook.java
+++ lesson7.3/gradebook1/GradeBook.java
@@ -30,20 +30,31 @@ public class GradeBook
     public double totalForStudent(int studentIndex)
     {
         // TODO: Return the total of all the scores for the student in row studentIndex
-        int sum = 0;
-        
-        if (studentIndex < grades.length)
-        {
-            for (int i = 0; i < grades[studentIndex].length; i++)
-            {
-                sum += grades[studentIndex][i];
-            }
-            return sum;
-        }
-        else
+//         int sum = 0;
+//         
+//         if (studentIndex < grades.length)
+//         {
+//             for (int i = 0; i < grades[studentIndex].length; i++)
+//             {
+//                 sum += grades[studentIndex][i];
+//             }
+//             return sum;
+//         }
+//         else
+//         {
+//             return -1;
+//         }
+        // Below solution is from Udacity
+        if (studentIndex < 0 || studentIndex >= numStudents)
         {
             return -1;
         }
+        double sum = 0;
+        for (int column = 0; column < numTopics; column++)
+        {
+            sum += grades[studentIndex][column];
+        }
+        return sum;
     }
 
     // This method reads the scores from the file. Don't look inside.
