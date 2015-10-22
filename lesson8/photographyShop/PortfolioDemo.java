2c59f0cda24e49f56af9a9287a4d78eaa50e8588
karnoq <smith.sheldon@gmail.com>
2015-10-22 13:55:43 -0600

    Created PortfolioDemo class to test Portfolio and FinishedPhoto classes

---
+++ lesson8/photographyShop/PortfolioDemo.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class PortfolioDemo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PortfolioDemo
+{
+    public static void main(String[] args)
+    {
+        Portfolio portfolio = new Portfolio();
+        portfolio.addFinishedWork("icesmall28.jpg", "Justin Black");
+        portfolio.addFinishedWork("ecuasmll5.jpg", "Justin Black");
+        protfolio.addFinishedWork("france6small.jpg", "Justing Black");
+        portfolio.displayFinishedWork();
+    }
+}
