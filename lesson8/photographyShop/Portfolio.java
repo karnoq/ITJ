90147cbde55fcee10faa34de184a48337fa9c89f
karnoq <smith.sheldon@gmail.com>
2015-10-22 13:38:07 -0600

    Building the Portfolio class. Added public constructor and methods addFinishedWork and displayFinishedwork

--- lesson8/photographyShop/Portfolio.java
+++ lesson8/photographyShop/Portfolio.java
@@ -5,5 +5,25 @@
 
 public class Portfolio
 {
-
+    ArrayList<FinishedPhoto> finishedWorks;
+    
+    public Portfolio()
+    {
+        finishedWorks = new ArrayList<FinishedPhoto>();
+    }
+    
+    public void addFinishedWork(String photoFileName, String photographer)
+    {
+        
+    }
+    
+    public void displayFinishedwork()
+    {
+        double nextEmptyPosition = 0;
+        for (FinishedPhoto photo : finishedWorks)
+        {
+            Picture photo = ...
+            Text signature = ...
+        }
+    }
 }
