980d85f31134a743c0ffcf6d86645c9804fd6985
karnoq <smith.sheldon@gmail.com>
2015-10-26 20:08:31 -0600

    Fixed a math mistake

--- lesson8/ps8/hw8b/draft_files/WindowShape.java
+++ lesson8/ps8/hw8b/draft_files/WindowShape.java
@@ -80,6 +80,6 @@ public class WindowShape
         // Math.PI * r * r = area of circle
         // width = 2*r -> r = width/2 -> r*r = width * width / 4
         // But we need half of the circle, so width*width/8
-        return width * height * Math.PI * width * width / 8;
+        return width * height + Math.PI * width * width / 8;
     }
 }
