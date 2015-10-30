801c8996c24eef342965416b885dc0f6e6274e8f
karnoq <smith.sheldon@gmail.com>
2015-10-29 21:09:52 -0600

    Class Dog is now Moveable; stubbed move method

--- lesson9/scene4/Dog.java
+++ lesson9/scene4/Dog.java
@@ -4,7 +4,7 @@
    They move one pixel downwards every second.
 */
 
-public class Dog implements Drawable
+public class Dog implements Drawable, Moveable
 {
     private Picture pic;
     private Text name;
@@ -21,5 +21,11 @@ public class Dog implements Drawable
     {
         pic.draw();
         name.draw();
+    }
+    
+    public void move(int seconds)
+    {
+        final int SPEED = 1; // pixels per second
+        pic.translate(0, SPEED * seconds);
     }
 }
